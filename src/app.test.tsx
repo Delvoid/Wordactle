ff73606403d4230a4667c99e3b44f4710de5dce6
Delvoid <delvoidltd@gmail.com>
2022-02-14 21:59:06 +0000

    add test

---
+++ src/app.test.tsx
@@ -0,0 +1,11 @@
+import { describe, expect, test } from 'vitest';
+import App from './App';
+import { render, screen, userEvent, within } from './test/test-utils';
+
+describe('Simple test', () => {
+  it('the title is visable', () => {
+    render(<App />)
+    expect(screen.getByText(/Reacdle/i)).toBeInTheDocument()
+    
+  });
+});
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/wordle/',
+  plugins: [react()],
+  // @ts-expect-error
+  test: {
+    global: true,
+    environment: 'jsdom',
+    setupFiles: './src/test/test-setup.ts',
+  },
 })
